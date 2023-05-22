11a988ef3de2442a65c46a9412efef7d9c453f31
Kahnu Charan Swain <djkanha044@gmail.com>
2023-05-22 18:21:09 +0530

    Arrows Issue Fixed

--- assets/js/sawss-slider-main.js
+++ assets/js/sawss-slider-main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function ($) {
     // Slick slider configuration options
     jQuery(".video-slider-section").slick({
         dots: true,
-        // arrow: false,
+        arrows: false,
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1

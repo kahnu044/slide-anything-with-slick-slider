661ce07a682f7a168d146ba1f8af12dd05f07673
Kahnu Charan Swain <djkanha044@gmail.com>
2023-05-23 12:53:58 +0530

    Js Updated

--- assets/js/sawss-slider-main.js
+++ assets/js/sawss-slider-main.js
@@ -1,12 +1,13 @@
 jQuery(document).ready(function ($) {
 
     // Slick slider configuration options
-    jQuery(".video-slider-section").slick({
+    jQuery(".your-slider-section").slick({
         dots: true,
         arrows: false,
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+		// vertical: true,
+		// verticalSwiping : true,
     });
-
 });
